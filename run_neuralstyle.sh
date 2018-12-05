140479863d3253ada1800bb32d4cac75ce0e5472
Audrey <44976838+audreyls@users.noreply.github.com>
2018-12-05 17:44:15 -0500

    Update run_neuralstyle.sh

--- run_neuralstyle.sh
+++ run_neuralstyle.sh
@@ -61,7 +61,11 @@
 	#     Default: N | Accepts: Y/N
 		origcolors=""
 	
-	# b10. Do you want the neural_style script to run verbosely--how many steps completed, loading model weights, etc.?
+	# b10. Do you want to match the parameters of the input and the style images? May lead to more cohesive stylization.
+	#	   Default: N | Accepts: Y/N
+		matchparams=""
+	
+	# b11. Do you want the neural_style script to run verbosely--how many steps completed, loading model weights, etc.?
 	#      Default: N | Accepts: Y/N
 		verbose=""
 	
@@ -134,6 +138,7 @@ export maxitupres
 export style_scale
 export size
 export origcolors
+export matchparams
 export verbose
 
 export waifu_algo
